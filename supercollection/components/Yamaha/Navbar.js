18b5cc3d25fa6530262b03464ca889deed006af8
Luca Rivoli <l.rivoli@donboscosandona.it>
2026-03-11 10:09:25 +0100

    11/03/2026

--- supercollection/components/Yamaha/Navbar.js
+++ supercollection/components/Yamaha/Navbar.js
@@ -1,46 +1,54 @@
-export default function Navbar(){
-    return (
-        <>
-        <div className="flex flex-col">
-        <div>
-            <div>
-<img src="/images/Yamaha_logo.png" alt="" className="w-30"/>
-            </div>
-            <div>
-<img src="/images/70_anniversary.png" alt="" className="w-10"/>
-            </div>
-            <div>
-<h1>i nostri modelli</h1>
-            </div>
-            <div>
-<h1>accessori</h1>
-            </div>
-            <div>
-<h1>abbigliamento & merchandise</h1>
-            </div>
-            <div>
-                <h1>eventi & experience</h1>
-            </div>
-            <div>
-<h1>servizi & manutenzione</h1>
-            </div>
-            <div>
-<img src="" alt="" />
-            </div>
-            <div>
-                <img src="" alt="" />
-            </div>
-            <div>
-<img src="" alt="" />
-            </div>
+export default function Navbar() {
+  return (
+    <>
+      <div className="flex justify-evenly border border-white bg-white fixed">
+        <div className="flex justify-around gap-2">
+          <div>
+            <img src="/images/Yamaha_logo.png" alt="" className="w-30 mt-2" />
+          </div>
+          <div>
+            <img src="/images/70_anniversary.png" alt="" className="w-10 mt-4" />
+          </div>
         </div>
-        <div>
-<img src="" alt="" />
-            </div>
-            <div>
-<img src="" alt="" />
-            </div>
+        
+        <div className="flex justify-evenly gap-10 mt-5">
+          <div>
+            <h1 className="font-bold">I NOSTRI MODELLI</h1>
+          </div>
+          <div>
+            <h1 className="font-bold">ACCESSORI</h1>
+          </div>
+          <div>
+            <h1 className="font-bold">ABBIGLIAMENTO & MERCHANDISE</h1>
+          </div>
+          <div>
+            <h1 className="font-bold">EVENTI & EXPERIENCE</h1>
+          </div>
+          <div>
+            <h1 className="font-bold">SERVIZI & MANUTENZIONE</h1>
+          
         </div>
-        </>
-    );
+</div>
+
+        <div className="flex justify-evenly gap-2 mt-2">
+          <div>
+            <img src="/images/lente.webp" alt="" className="w-10" />
+          </div>
+          <div>
+            <img src="/images/ping.webp" alt="" className="w-10" />
+          </div>
+          <div>
+            <img src="/images/Carrello.png" alt="" className="w-10" />
+          </div>
+          <div>
+            <img src="/images/account.png" alt="" className="w-10" />
+          </div>
+
+          <div>
+            <img src="/images/bandiera.png" alt="" className="w-10" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
