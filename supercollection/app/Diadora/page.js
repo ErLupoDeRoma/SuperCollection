70dd65d2b943efe507b3911f3a261ebcadd4b535
Luca Rivoli <l.rivoli@donboscosandona.it>
2026-04-24 11:03:36 +0200

    24/04/2026

--- supercollection/app/Diadora/page.js
+++ supercollection/app/Diadora/page.js
@@ -2,6 +2,8 @@ import Navbar from "@/components/Diadora/Navbar";
 import Card1 from "@/components/Diadora/Card1";
 import Card2 from "@/components/Diadora/Card2";
 
+import Card4 from "@/components/Diadora/Card4";
+
 export default function Diadora() {
   return (
     <>
@@ -10,6 +12,8 @@ export default function Diadora() {
 <Card1 immagine=" bg-[url(/images/Diadora/Scarpa_grande.jpg)]" sottotitolo="Running collection" titolo="Il cuore del running italiano" bottone="Scopri Atomo Star" colore=" text-white" coloreb=" text-black"/>
 <Card2 titolo="Le nostre collezioni" riquadro1="Uomo" riquadro2="Donna" riquadro3="Sport" riquadro4="Heritage" riquadro5="Sportswear"/>
 <Card1 immagine=" bg-[url(/images/Diadora/Tipa_con_giubbotto.jpg)]" sottotitolo="Heritage collection" titolo="Oltre la Pista" bottone="Scopri Equipe Revenge" colore=" text-black" coloreb=" text-black"/>
-    </>
+<Card4 titolo="Lifestyle classic" riquadro1="Prestige" riquadro2="Trainer" riquadro3="B.560" riquadro4="B.Elite" riquadro5="Game" riquadro6="Tokio" riquadro7="Mythos" riquadro8="Cross" riquadro9="Equipe" riquadro10="Camaro"/>
+<Card1 immagine=" bg-[url(/images/Diadora/Tipa_sdraiata.jpg)]" sottotitolo="Sportwear" titolo="The Future is Bright" bottone="Scopri Mythos 280" colore=" text-white" coloreb=" text-black"/>
+   </>
   );
 }
---
+++ supercollection/components/Diadora/Card4.js
@@ -0,0 +1,44 @@
+export default function Card4(C4) {
+  return (
+    <>
+      <div className="flex flex-col items-center gap-6 mt-10 mb-10">
+        <h1 className="text-5xl font-bold text-center">
+          {C4.titolo}
+        </h1>
+        <div className="flex flex-row gap-3 text-[#383935] text-center">
+          <h1 className=" rounded-full px-5 py-2 font-extrabold">
+            <img src="/images/Diadora/Tokio.png" className="w-30 flex" />
+            {C4.riquadro1}
+          </h1>
+          <h1 className="rounded-full px-5 py-2 font-extrabold">
+            {C4.riquadro2}
+          </h1>
+          <h1 className="rounded-full px-5 py-2 font-extrabold">
+            {C4.riquadro3}
+          </h1>
+          <h1 className="rounded-full px-5 py-2 font-extrabold">
+            {C4.riquadro4}
+          </h1>
+          <h1 className="rounded-full px-5 py-2 font-extrabold">
+            {C4.riquadro5}
+          </h1>
+          <h1 className="rounded-full px-5 py-2 font-extrabold">
+            {C4.riquadro6}
+          </h1>
+          <h1 className="rounded-full px-5 py-2 font-extrabold">
+            {C4.riquadro7}
+          </h1>
+          <h1 className="rounded-full px-5 py-2 font-extrabold">
+            {C4.riquadro8}
+          </h1>
+          <h1 className="rounded-full px-5 py-2 font-extrabold">
+            {C4.riquadro9}
+          </h1>
+          <h1 className="rounded-full px-5 py-2 font-extrabold">
+            {C4.riquadro10}
+          </h1>
+        </div>
+      </div>
+    </>
+  );
+}
